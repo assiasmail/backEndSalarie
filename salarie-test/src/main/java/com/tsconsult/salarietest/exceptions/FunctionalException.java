2a6aa302bbc2c63f2506ab35a97803946d3e3f20
Assia <Assia@DESKTOP-JSN9EN7>
2019-12-10 15:25:26 +0100

    BackEnd de dedoublement(remonter les exception fonctionnelles, mettre la classe dans un package, ajout de la classe FunctionException qui hérite de RuntimeException et utilisation d'un Switch et des énumérations)

---
+++ salarie-test/src/main/java/com/tsconsult/salarietest/exceptions/FunctionalException.java
@@ -0,0 +1,18 @@
+package com.tsconsult.salarietest.exceptions;
+
+public class FunctionalException extends RuntimeException  {
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FunctionalException(String message) {
+		super(message);
+		
+	}
+	
+	
+
+}
